7f0a9a4481e9bf74efdd6df554692a1a7851411e
Joey K Black <joeykblack@gmail.com>
2016-09-18 14:47:44 -0400

    Turned test.xml into template

--- SpiritualGifts/util/xmlToProp.py
+++ SpiritualGifts/util/xmlToProp.py
@@ -9,12 +9,15 @@ import re
 
 if __name__ == '__main__':
     pattern = re.compile('question\snum="(\d+)"\scategory="(\w+)"\sgifting="(\w+)"')
-    with open('xml/test-lg.xml') as f:
+    with open('xml/test-en.xml') as f:
         key=None
         for line in f:
             match = pattern.search(line)
             if match:
                 key = match.group(3)+"_"+match.group(2)+"_Question_"+match.group(1)
+                print line.rstrip()
             elif key:
-                print key + " = " + line.strip()
+                print '\t\t$' + key
                 key = None
+            else:
+                print line.rstrip()
